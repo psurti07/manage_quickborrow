4e0742d57d365039ff35b7ec9c8a5e79daaf8dbe
vw-uday <verloop.dev4@gmail.com>
2026-08-24 10:46:48 +0530

    sendorderdata bizfin issue

--- app/Http/Controllers/SyncDataController.php
+++ app/Http/Controllers/SyncDataController.php
@@ -52,7 +52,7 @@ class SyncDataController extends Controller
     public function getInvoiceData($fields){
         try{
             switch($fields->product){
-                case 'SELF APPLY':
+                case 'SELFAPPLY':
                     $accType = 1;
                     break;
                 case 'HIRE AGENT':
@@ -104,9 +104,9 @@ class SyncDataController extends Controller
                         'inv_igst'          => $row->inv_igst,
                         'inv_grandtotal'    => $row->inv_grandtotal,
                         // custom fields
-                        'company_code'      => config('constants.company_code'),
+                        'company_code'      => 'QIKBRW5678',
                         'company_local_ip'  => '190.92.174.183',
-                        'product_code'      => (($row->acc_type == 1) ? 'SELF APPLY' : 'HIRE AGENT'),
+                        'product_code'      => (($row->acc_type == 1) ? 'SELFAPPLY' : 'HIREAGENT'),
                     ];
                 });
             
